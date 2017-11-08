d50cb1e61a68e0218bb2da0a508ba135cd3f9485
kschlunz <schlunzk@gmail.com>
2017-11-08 22:14:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let currentUser = "John";
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser;
-let upperCaseCurrentUser = 'John';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser;
+
